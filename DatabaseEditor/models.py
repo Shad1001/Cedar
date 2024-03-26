002257b8127a3afc48d2beeb096822621fbed7bb
Smuppers <91326388+Smuppers@users.noreply.github.com>
2024-03-26 10:39:35 -0400

    Added Database Sections
    Added Customers, Employees, OrderDetails, Food, and AddOns onto the Database.

--- DatabaseEditor/models.py
+++ DatabaseEditor/models.py
@@ -1,6 +1,41 @@
 from config import db
 import time
 
+class Employees(db.Model):
+    employeeID = db.Column(db.Integer, primary_key=True)
+    lastName = db.Column(db.String(100), nullable=False)
+    firstName = db.Column(db.String(100), nullable=False)
+    dob = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phoneNumber = db.Column(db.String(100), nullable=False)
+
+    def to_json(self):
+        return{
+            "employeeID": self.employeeID,
+            "lastName": self.lastName,
+            "firstName": self.firstName,
+            "dob": self.dob,
+            "email": self.email,
+            "phoneNumber": self.phoneNumber
+        }
+
+class Customers(db.Model):
+    customerID = db.Column(db.Integer, primary_key=True)
+    lastName = db.Column(db.String(100), nullable=False)
+    firstName = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phoneNumber = db.Column(db.String(100), nullable=False)
+
+    def to_json(self):
+        return {
+            "customerID": self.customerID,
+            "lastName": self.lastName,
+            "firstName": self.firstName,
+            "email": self.email,
+            "phoneNumber": self.phoneNumber
+        }
+    
+
 class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -34,3 +69,45 @@ class OrderItem(db.Model):
             "tax": self.tax,
             "total": self.total
         }
+    
+class OrderDetails(db.Model):
+    purchaseID = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    extendedPrice = db.Column(db.Float, nullable=False)
+
+    def to_json(self):
+        return{
+            "purchaseID": self.purchaseID,
+            "sku": self.sku,
+            "quantity": self.quantity,
+            "price": self.price,
+            "extendedPrice": self.extendedPrice
+        }
+
+class Food(db.Model):
+    SKU = db.Column(db.Integer, primary_key=True)
+    skuDescription = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+
+    def to_json(self):
+        return{
+            "SKU": self.SKU,
+            "skuDescription": self.skuDescription,
+            "price": self.price,
+            "category": self.category
+        }
+    
+
+class AddOns(db.Model):
+    SKU = db.Column(db.Integer, primary_key=True)
+    skuDescription = db.Column(db.String(100), primary_key=True)
+
+    def to_json(self):
+        return{
+            "SKU": self.SKU,
+            "skuDescription": self.skuDescription
+        }
+
