882a5047fe320c77df8de9fcace090349709e60a
Smuppers <91326388+Smuppers@users.noreply.github.com>
2024-03-27 14:00:38 -0400

    update main.py
    updated

--- DatabaseEditor/main.py
+++ DatabaseEditor/main.py
@@ -10,14 +10,14 @@ def admin():
 
 @app.route('/', methods=['GET','POST'])
 def add_food_item():
-    data = request.get_json
+    data = request.get_json()
 
     if 'name' not in data or 'price' not in data or 'category' not in data:
         return jsonify({"error": "Missing data for name, price or category"}), 400
     con = sqlite3.connect('mydatabase.db')
     cur = con.cursor()
     # new_item = MenuItem(name=data['name'], id=data['id'], price=data['price'], category=data['category'])
-    cur.execute("INSERT INTO menu_item (id, name, price, category) VALUES( id=data['id'] name=data['name'] price=data['price'] category=data['category'] ")
+    cur.execute("INSERT INTO menu_item (id, name, price, category) VALUES( id=data['id'], name=data['name'], price=data['price'], category=data['category'] ")
     #cur.execute("SELECT id, name, price, category FROM menu_item;")
     db.session.commit()
     return jsonify({"message": "Food item added successfully."}), 201
