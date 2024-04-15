e06eb4dcecd7ad0e9de5a0e45e90b84263c53a8c
mishurac <mishurac@go.stockton.edu>
2024-04-15 19:22:14 -0400

    Update cart.py
    Updated the routing for the cart functionalities.

--- cedar 2/cart.py
+++ cedar 2/cart.py
@@ -1,12 +1,23 @@
-#from app import db
-# @ routes  *insert something about route here
-#def add_to_cart():
-  #  cart = []
+from flask import Flask, render_template, request, redirect, session, url_for
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 
-#@ route //insert something about routes here i.e "GET", "PUT" etc
+#@app.route('/initialize-cart', methods=['POST'])
+#def initialize_cart():
+
+#cart = []
+    
+#@app.route('/add-to-cart', methods=['POST'])
+#def add_to_cart():
+    
+              
+#@app.route('/remove-from-cart', methods=['POST'])
 #def delete_cart_item():
 
-#@route *insert route 
+
+#@app.route('/order-confirmation', methods=['GET'])
 #def order_confirmation():
     # after an order is finished this saves the final state the cart to the database
     # when the user finishs an order this displays as their receipt and will also show up in their user dashboard
+
+if __name__ == '__main__':
+    app.run(debug=True)
