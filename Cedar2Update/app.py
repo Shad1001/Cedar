537aad50c8222d161f59b12a53ead191c3480527
mangogreentea1 <158528794+mangogreentea1@users.noreply.github.com>
2024-04-18 13:31:58 -0400

    Create app.py

---
+++ Cedar2Update/app.py
@@ -0,0 +1,148 @@
+
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///databasefilee.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Food(db.Model):
+    foodID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.BLOB)
+
+class FoodItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(255))  
+
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False) 
+     
+    
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/signup', methods=['GET'])
+def signup_form():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    
+    fullname = request.form['fullname']
+    username = request.form['username']
+    password = request.form['password']
+
+    
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        
+        return "Username already taken. Please choose another username."
+
+    
+    new_user = User(fullname=fullname, username=username, password=password)
+
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    
+    return redirect(url_for('login'))
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        
+        user = User.query.filter_by(username='admin').first()  
+        if user:
+            user.isAdmin = True  
+            db.session.commit()
+
+        
+        if user and user.password == password:
+            if user.isAdmin == True :
+                return render_template('admin.html') 
+         
+        else:
+            return redirect(url_for('menu')) #SET ANY PAGE HERE. I JUST PUT MENU AS A PLACEHOLDER FOR REGULAR USERS
+    
+    return render_template('login.html')
+
+
+
+@app.route('/add-food-item', methods=['POST'])
+def add_food_item():
+    if request.method == 'POST':
+        name = request.form['name']
+        price = request.form['price']
+        category = request.form['category']
+        image_url = request.form.get('image_url', '') 
+        
+        new_item = FoodItem(name=name, price=float(price), category=category, image_url=image_url)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect(url_for('admin'))
+    
+    return redirect(url_for('admin'))  
+
+
+
+@app.route('/delete-food-item', methods=['POST'])
+def delete_food_item():
+    item_id = request.form['item_id']
+    name = request.form['name']
+    price = request.form['price']
+    category = request.form['category']
+
+   
+    item = FoodItem.query.filter_by(id=item_id, name=name, price=price, category=category).first()
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('admin'))  
+   
+
+@app.route('/menu')
+def menu():
+    food_items = Food.query.order_by(Food.category).all()  
+    return render_template('menu.html', food_items=food_items)
+
+
+@app.route('/specials')
+def show_specials():
+    food_itemss = FoodItem.query.all()
+    return render_template('specials.html', food_items=food_itemss)
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  
+    app.run(debug=True)
